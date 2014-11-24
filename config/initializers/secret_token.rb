6bfadc83297d54185cb6edae9756af8de65e82dc
Chenyan Lu <maggkey@gmail.com>
2014-11-24 01:59:29 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diary::Application.config.secret_token = 'dd37007fcabce1b8ecff646c3029dac57fc7abfd15fe91261c9beb4cc574f39977e77cbdf55c57534fa9ead8609d903c38462997de1f08f650b6ae7ced750fa2'
